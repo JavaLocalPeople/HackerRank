9317d56f38d5da3c99983bc6cd884cc16adff69b
JavaLocalPeople <yuchaiyuchai@gmail.com>
2017-06-30 13:58:17 -0500

    Update main.cpp

--- Data Structures/Linked Lists/Reverse A Linked List/main.cpp
+++ Data Structures/Linked Lists/Reverse A Linked List/main.cpp
@@ -1,15 +1,26 @@
+// PASS
+
 /*
   Reverse a linked list and return pointer to the head
   The input list will have at least one element  
   Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
 */
-struct Node
-{
-   int data;
-   struct Node *next;
-}
-
 Node* Reverse(Node *head)
 {
-  // Complete this method
+    Node *cur = head;
+    Node *nextNode = new Node();
+    Node *pre = NULL;
+    while (cur != NULL)
+    {
+        nextNode = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = nextNode;
+    }
+    return pre;
 }
